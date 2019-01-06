f6e2c4360b4bb9c4b60234b8c4277044519b80f3
James Woolfenden <james.woolfenden@gmail.com>
2019-01-06 01:30:45 +0000

    reset tags

--- examples/exampleA/module.codecommit.tf
+++ examples/exampleA/module.codecommit.tf
@@ -1,6 +1,6 @@
 module "codecommit" {
   source          = "JamesWoolfenden/codecommit/aws"
-  version         = "0.0.9"
+  version         = "0.0.4"
   default_branch  = "${var.default_branch}"
   repository_name = "${var.repository_name}"
 }
