893f86dd04550052f02fa5fdb61104048e5a974d
James Woolfenden <james.woolfenden@gmail.com>
2019-01-06 01:25:33 +0000

    use new tag version

--- examples/exampleA/module.codecommit.tf
+++ examples/exampleA/module.codecommit.tf
@@ -1,6 +1,6 @@
 module "codecommit" {
   source          = "JamesWoolfenden/codecommit/aws"
-  version         = "0.0.8"
+  version         = "0.0.9"
   default_branch  = "${var.default_branch}"
   repository_name = "${var.repository_name}"
 }
