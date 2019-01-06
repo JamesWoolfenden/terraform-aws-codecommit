1e57649b68cf8dd70d88142be6ccc4f814c0aadd
James Woolfenden <james.woolfenden@gmail.com>
2019-01-06 01:19:47 +0000

    Fix docs

--- examples/exampleA/module.codecommit.tf
+++ examples/exampleA/module.codecommit.tf
@@ -1,6 +1,6 @@
 module "codecommit" {
   source          = "JamesWoolfenden/codecommit/aws"
-  version         = "0.0.5"
+  version         = "0.0.8"
   default_branch  = "${var.default_branch}"
   repository_name = "${var.repository_name}"
 }
