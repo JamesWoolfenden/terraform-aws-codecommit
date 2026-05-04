28f842c58977d75892724f656bee190da6622054
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:29:56 +0100

    fix: skip CKV_AWS_290 and CKV_AWS_355 on IAM policy resources

--- aws_iam_policy.restrictmaster.tf
+++ aws_iam_policy.restrictmaster.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_policy" "restrictmaster" {
+  # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
+  # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   count  = var.developer_group == "" ? 0 : 1
   policy = data.aws_iam_policy_document.restrictmaster.json
 }
--- data.aws_iam_policy_document.sns_topic_policy.tf
+++ data.aws_iam_policy_document.sns_topic_policy.tf
@@ -1,5 +1,7 @@
 data "aws_caller_identity" "current" {}
 data "aws_iam_policy_document" "sns_topic_policy" {
+  # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
+  # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   statement {
     effect = "Allow"
 
