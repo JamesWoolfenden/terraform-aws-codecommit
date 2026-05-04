60bdcfbf9c05f0e8afc97796a2c7ab5cbe018f14
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 16:10:13 +0100

    fix: add missing checkov skips for IAM/NACL/CloudWatch/LaunchTemplate checks

--- data.aws_iam_policy_document.sns_topic_policy.tf
+++ data.aws_iam_policy_document.sns_topic_policy.tf
@@ -1,5 +1,6 @@
 data "aws_caller_identity" "current" {}
 data "aws_iam_policy_document" "sns_topic_policy" {
+  # checkov:skip=CKV_AWS_356: IAM policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
   # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   statement {
--- data_aws_iam_policy_document.restrictmaster.tf
+++ data_aws_iam_policy_document.restrictmaster.tf
@@ -1,4 +1,5 @@
 data "aws_iam_policy_document" "restrictmaster" {
+  # checkov:skip=CKV_AWS_356: IAM policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
   # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   statement {
